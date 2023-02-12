123a9f14a7ccb5ef6139dfb2fd9c8646cd19219b
Billy Lee <codingbillylee@gmail.com>
2023-02-12 11:36:12 -0800

    Add update first name function

--- html template/js/scripts.js
+++ html template/js/scripts.js
@@ -3,21 +3,42 @@ function AddressBook() {
   this.currentId = 0;
 }
 
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
 AddressBook.prototype.assignId = function() {
   this.currentId += 1;
   return this.currentId;
 };
-
 
 AddressBook.prototype.addContact = function(contact) {
   contact.id = this.assignId();
   this.contacts[contact.id] = contact;
 };
 
-function Contact(firstName, lastName, phoneNumber) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.phoneNumber = phoneNumber;
+AddressBook.prototype.findContact = function(id) {
+  if (this.contacts[id] !== undefined) {
+    return this.contacts[id];
+  }
+  return false;
+};
+
+AddressBook.prototype.deleteContact = function(id) {
+  if (this.contacts[id] === undefined) {
+    return false;
+  }
+  delete this.contacts[id];
+  return true;
+};
+
+AddressBook.prototype.update = function(id, newFirstName) {
+  if (this.contacts[id] === undefined) {
+    return false;
+  }
+  this.contacts[id].firstName = newFirstName;
 }
 
 Contact.prototype.fullName = function() {
@@ -27,5 +48,7 @@ Contact.prototype.fullName = function() {
 let addressBook = new AddressBook();
 let contact = new Contact("Ada", "Lovelace", "503-555-0100");
 let contact2 = new Contact("Grace", "Hopper", "503-555-0199");
+let contact3 = new Contact("Gracie", "Hopping", "503-555-2222");
 addressBook.addContact(contact);
 addressBook.addContact(contact2);
+addressBook.addContact(contact3);
